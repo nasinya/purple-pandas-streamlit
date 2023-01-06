c3e4c7b93f14df0e0fba3570a3bc8e3a14195d0c
AnthonyCampbell208 <78286293+AnthonyCampbell208@users.noreply.github.com>
2023-01-06 10:21:39 -0500

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -4,7 +4,7 @@ import streamlit as st
 import statsmodels.api as sm
 import plotly.graph_objects as go
 
-st.set_page_config(page_title="Main Analysis")
+# st.set_page_config(page_title="Main Analysis")
 
 cedf = pd.read_csv("CO2 Emissions_Canada.csv")
 
